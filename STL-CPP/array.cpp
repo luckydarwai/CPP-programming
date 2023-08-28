4ebe2db20c15fa7e582e25990cdabde20a5eacfa
luckydarwai <luckydarvai545@gmail.com>
2023-08-28 10:31:32 +0530

    stl array

---
+++ STL-CPP/array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<array>
+using namespace std;
+int main()
+{
+   array<int,5> arr={1,2,3,4,5};
+   int s=arr.size();
+   for (int i = 0; i < s; i++)
+   {
+    cout<<arr[i]<<endl;
+   }
+   
+   cout<<"get element at index using stl array : "<<endl;
+   cout<<arr.at(2)<<endl;
+
+   cout<<"check array empty or not : "<<arr.empty()<<endl;
+   
+
+}
